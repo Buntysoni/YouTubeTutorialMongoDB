55f8847d03952f4d8fd8b5b7bcc3f41240ba1c26
Bunty Soni <buntysonimail@gmail.com>
2023-07-08 14:32:06 +0530

    Add project files.

---
+++ YouTubeTutorialMongoDB/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using YouTubeTutorialMongoDB.Models;
+
+namespace YouTubeTutorialMongoDB.Controllers
+{
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly IMongoCollection<Users> _usr;
+        public HomeController()
+        {
+            var client = new MongoClient("mongodb://localhost:27017");
+            var database = client.GetDatabase("YouTubeTutorial");
+            _usr = database.GetCollection<Users>("table1");
+        }
+
+        [HttpGet("GetUsers")]
+        public IActionResult GetUsers()
+        {
+            var data = _usr.Find<Users>(x => x.firstname != null).ToList();
+            return Ok(data);
+        }
+
+        [HttpPost("InsertUser")]
+        public IActionResult InsertUser(Users model)
+        {
+            model.UserId = _usr.AsQueryable().Any() ? _usr.AsQueryable().OrderByDescending(x => x.UserId).FirstOrDefault().UserId + 1 : 1;
+
+            _usr.InsertOne(model);
+            return Ok(model);
+        }
+        
+        [HttpPost("InsertManyUser")]
+        public IActionResult InsertManyUser(List<Users> model)
+        {
+            _usr.InsertMany(model);
+            return Ok(model);
+        }
+    }
+}
---
+++ YouTubeTutorialMongoDB/Models/Users.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace YouTubeTutorialMongoDB.Models
+{
+    public class Users
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string? _id { get; set; }
+        public int UserId { get; set; }
+        public string? firstname { get; set; }
+        public string? lastname { get; set; }
+    }
+}
